a84ab4f051493a010812539be22f893072432a9d
Titouan Lazard <tlazard@lexfo.fr>
2022-03-13 17:22:15 +0100

    First Support for swaysocket

--- sway_eww.py
+++ sway_eww.py
@@ -5,15 +5,16 @@ import socket
 import subprocess
 import json
 import shutil
+import struct
 
 verbose = False
 sock_path = "./sock"
-eww_mode = False
 
-
-CMD_QUIT="quit"
-CMD_REFRESH="refresh"
-
+ipc_magic='i3-ipc';
+subscribe_type = 0x02
+get_outputs = 0x03
+get_workspaces = 0x01
+header_length = 14
 
 def log(s):
     if verbose:
@@ -25,112 +26,120 @@ def error_exit(s):
     sys.exit()
 
 
-        
-class Client():
-    def __init__(self, sock_path):
-        if  not os.path.exists(sock_path):
-            error_exit("No deamon at this address...")
-        else:
-            self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-            self.sock.connect(sock_path)
-
-    def recv(self):
-        return self.sock.recv(1024)
-    
-    def send(self, s):
-        self.sock.send(s.encode())
-        
-    def kill(self):
-        log("Slay the daemon")
-        self.send(CMD_QUIT)
-
-    def refresh(self):
-        log("Refresh")
-        self.send(CMD_REFRESH)
-
 
 
 class Workspace():
-    def __init__(self, wp):
-        self.name = wp["name"]
-        self.focused = wp["focused"]
-        self.output = wp["output"]
-
+    def __init__(self, wp_json, daemon):
+        self.num = wp_json["num"]
+        self.daemon = daemon
+        self.output = wp_json["output"]
+        self.daemon.outputs[self.output] = self.num
+        
+        
     def __repr__(self):
-        return self.name + '@' + self.output
+        return str(self.num) + '@' + self.output + (("-focused") if self.daemon.focused_wp==self.num else "-not_focused") + (" - VISIBLE" if self.daemon.outputs[self.output] == self.num else " - NOT VISIBLE" )
 
-    def write_as_button(self):
-        return f"(button :class \"button-workspace{'-focused' if self.focused else ''}\" :onclick \"swaymsg workspace {self.name} /home/titouan/.config/eww/scripts/sway_eww.py -r\" \"{self.name}\" )"
-        
+
+    
 class Daemon():
-    
-    def recv(self):
-        return self.current_cli.recv(1024)
-    
-    def send(self, s):
-        self.current_cli.send(s.encode())    
-        
-    def quit(self, cmd):
-        self.exit = True
 
-    def refresh(self, cmd):
-        self.wps = []
-        ret = subprocess.run(["swaymsg", "-t", "get_workspaces"], capture_output=True, env=os.environ.copy())
-        js = json.loads(ret.stdout.decode())
-        
-        for wp in js:
-            if wp['type'] != "workspace":
-                continue
+    def update(self, data):        
+        i = json.loads(data.decode())
+        if "change" in i:
+            if i["change"] == "focus":
             
-            self.wps.append(Workspace(wp))
-        if self.eww_mode:
-            self.render_eww()
-
-    def render_eww(self):
-        for wp in self.wps:
-            if wp.output not in self.output_files:
-                self.output_files[wp.output] = [open(f"/tmp/sway/{len(self.output_files)}", "w"),""]
-            self.output_files[wp.output][1] += wp.write_as_button()
-        self.output_files[wp.output][1] = "(box :space-evenly false :halign 'center' :class 'workspaces' " + self.output_files[wp.output][1] + ")\n"
-        for f in self.output_files:
-            self.output_files[f][0].write(self.output_files[f][1])
-            self.output_files[f][0].flush()
-            self.output_files[f][1] = ""
+                if "container" in i:
+                    # change focused window
+                    self.current_name = i["container"]["name"]
+                elif "current" in i:
+                    # change focused workspace
+                    self.focused_wp = int(i["current"]["name"])
+                    self.outputs[i["current"]["output"]] = int(i["current"]["name"])
+                else:
+                    print(json.dumps(i, indent=4))
+            elif i["change"] == "move":
+                # Moving window to other workspace
+                print(json.dumps(i, indent=4))
+            elif i["change"] == "init":
+                # Creating a new workspace
+                self.workspaces.append(Workspace(i["current"], self))
+            elif i["change"] == "empty":
+                # Closing an empty workspace
+                for wp in range(len(self.workspaces)):
+                    if self.workspaces[wp].num == i["current"]["num"]:
+                        self.workspaces.remove(self.workspaces[wp])
+                    
+            else:
+                print(i["change"])
             
-        
     def loop(self):
         while not self.exit:
-            self.current_cli,_ = self.sock.accept()
-            data = self.recv().decode()
-            try:
-                cmd = data.split(" ")
-                self.method_name[cmd[0].rstrip()](cmd)
-            except Exception as e:
-                log("Error" + str(e))
+            data = self.sock.recv(header_length) 
+            size, t = struct.unpack_from("<II", data, offset=len(ipc_magic))
+            data = self.sock.recv(size)
+            self.update(data)
         self.sock.close()
 
-    def __init__(self, sock_path, eww_mode):
-        self.eww_mode = eww_mode
-        self.method_name = {CMD_QUIT:self.quit, CMD_REFRESH: self.refresh}
-        self.wps = []
-        try:
-            os.unlink(sock_path)
-        except OSError:
-            if os.path.exists(sock_path):
-                error_exit("Socket exist already and cannot be deleted")
+    def init_sock_subscription(self):
+        log("Initialize log subscription")
+        payload = "['workspace','window']"
+        mess_type = struct.pack("<I", subscribe_type)
+        size = len(payload)
+        header = ipc_magic.encode()
+        buf = ipc_magic.encode() + struct.pack("<I",size) + mess_type
+        self.sock.send(buf)
+        self.sock.send(payload.encode())
+        data = self.sock.recv(header_length) 
+        size, t = struct.unpack_from("<II", data, offset=len(ipc_magic))
+        data = self.sock.recv(size)
+        
 
-        self.current_cli = None
+    def initial_state(self):
+        header = ipc_magic.encode()
+        mess_type = struct.pack("<I", get_outputs)
+        buf = ipc_magic.encode() + struct.pack("<I",0) + mess_type
+        self.sock.send(buf)
+        data = self.sock.recv(header_length) 
+        size, t = struct.unpack_from("<II", data, offset=len(ipc_magic))
+        data = self.sock.recv(size)
+        js = json.loads(data.decode())
+        self.outputs = {o['name']:int(o['current_workspace']) for o in js}
+
+
+        header = ipc_magic.encode()
+        mess_type = struct.pack("<I", get_workspaces)
+
+        buf = ipc_magic.encode() + struct.pack("<I",0) + mess_type
+        self.sock.send(buf)
+        data = self.sock.recv(header_length) 
+        size, t = struct.unpack_from("<II", data, offset=len(ipc_magic))
+        data = self.sock.recv(size)
+        js = json.loads(data.decode())
+        
+        for wp in js:
+            print(wp)
+            self.workspaces.append(Workspace(wp,self))
+            if(wp["focused"]):
+                self.focused_wp = wp['num']
+            
+            
+    def __init__(self, sock_path=None):
+        self.exit = False
+        self.workspaces = []
+        self.outputs = []
+        if sock_path == None:
+            sock_path = os.getenv("SWAYSOCK")
+        if sock_path == None:
+            error_exit("Cannot find sway socket")
         self.sock_path = sock_path
         self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        self.sock.bind(sock_path)
-        self.sock.listen(1)
-        self.exit = False
-        self.output_files = {}
-        
-        
+        self.sock.connect(sock_path)
+        self.init_sock_subscription()
+        self.initial_state()
             
     
 if __name__ == "__main__":
+    sock_path = None
     if "-v" in sys.argv:
         verbose = True
     if "-s" in sys.argv:
@@ -138,21 +147,10 @@ if __name__ == "__main__":
             sock_path = sys.argv[sys.argv.index("-s")+1]
         except:
             error_exit("Missing Socket Path")
-    if "-e" in sys.argv:
-        eww_mode = True
-        
+
     if "-d" in sys.argv:
         log("Preparing FS")
-        if os.path.exists("/tmp/sway"):
-            shutil.rmtree("/tmp/sway")
-        os.mkdir("/tmp/sway")
         log("Starting swayeww as deamon")
-        d = Daemon(sock_path, eww_mode)
+        d = Daemon(sock_path=sock_path)
         d.loop()
         log("Daemon Exit")
-    else:
-        c = Client(sock_path)
-        if "-k" in sys.argv:
-            c.kill()
-        elif "-r" in sys.argv:
-            c.refresh()
